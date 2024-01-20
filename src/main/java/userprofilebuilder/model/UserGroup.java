c3a2f3eedd12760af8e72762cef8a57c1645aa7d
Denis <Denis@192.168.0.198>
2024-01-20 16:29:54 +0000

    added ArrayList

--- src/main/java/userprofilebuilder/model/UserGroup.java
+++ src/main/java/userprofilebuilder/model/UserGroup.java
@@ -4,11 +4,34 @@
  */
 package userprofilebuilder.model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author 
  * This is model class may be handy for putting data relevant groups of User Profiles in {many Users]
  */
 public class UserGroup {
+    ArrayList<User> userGroup = new ArrayList<>();
+    private User[] users;
+    //constructor
     
+    /*public UserGroup(User[] users)
+    {
+       for(int i = 0; i < users.length; i++)
+       {
+           userGroup.add(users[i]);
+       }
+    }*/
+    
+
+    //getters and setters
+    
+    public ArrayList<User> getUserGroup() {
+        return userGroup;
+    }
+
+    public void setUserGroup(User user) {
+        userGroup.add( user);
+    }
 }
