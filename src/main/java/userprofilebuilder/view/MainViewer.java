479bc438cb2bd05cbbc869f298cf1c491bfba240
Denis <Denis@192.168.0.198>
2024-01-21 00:37:10 +0000

    W1R2 - Configured a basic Main Viewer and completed task

--- src/main/java/userprofilebuilder/view/MainViewer.java
+++ src/main/java/userprofilebuilder/view/MainViewer.java
@@ -4,11 +4,27 @@
  */
 package userprofilebuilder.view;
 
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
 /**
  *
  * @author 
  * This might be useful for defining you Main App Viewer e.g. a JFrame
  */
-public class MainViewer {
+public class MainViewer extends JFrame 
+{
+    
+    public void myGui()
+    {
+        JFrame appFrame = new JFrame("User Profile Builder");
+        appFrame.setSize(600,400);
+        appFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        appFrame.setVisible(true);
+        
+    }
+ 
+    
+    
     
 }
