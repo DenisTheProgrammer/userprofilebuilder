ecd30d6de4810c07ee0d209416ca2176985d95a3
Denis <Denis@192.168.0.198>
2024-01-23 22:14:15 +0000

    W1R3 - finished final task - GUI complete

--- src/main/java/userprofilebuilder/view/MainViewer.java
+++ src/main/java/userprofilebuilder/view/MainViewer.java
@@ -4,8 +4,22 @@
  */
 package userprofilebuilder.view;
 
+import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.util.ArrayList;
+import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTabbedPane;
 import javax.swing.WindowConstants;
+import javax.swing.border.Border;
+import userprofilebuilder.model.User;
+
 
 /**
  *
@@ -15,15 +29,58 @@ import javax.swing.WindowConstants;
 public class MainViewer extends JFrame 
 {
     
-    public void myGui()
+    public void myGui(ArrayList<User> myList)
     {
         JFrame appFrame = new JFrame("User Profile Builder");
+        appFrame.setLayout(new BorderLayout());
+        
+        /*JPanel topPan = new JPanel();
+        appFrame.add(topPan);*/
+
+        
+        JTabbedPane tabs = new JTabbedPane();
+        tabs.add("User Name", rootPane);
+        appFrame.add(tabs, BorderLayout.PAGE_START);
+        
+        
+        JPanel midPan = new JPanel();
+        midPan.setLayout(new GridBagLayout());
+        appFrame.add(midPan, BorderLayout.CENTER);
+        
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        gbc.fill = GridBagConstraints.VERTICAL;
+        gbc.insets = new Insets(5, 5, 5, 5);
+        
+        Border border = BorderFactory.createTitledBorder("Name");
+        midPan.setBorder(border);
+        
+        
+       
+        
+        
+        ButtonGroup radButOns = new ButtonGroup();
+              
+        for(int i = 0; i < myList.size();i++)
+        {
+            JPanel butPanel = new JPanel();
+            JRadioButton selButton = new JRadioButton(String.valueOf(myList.get(i)));
+            JButton editButton = new JButton("Edit");
+            JButton delButton = new JButton("Delete");
+            butPanel.add(selButton);
+            butPanel.add(editButton);
+            butPanel.add(delButton);
+            radButOns.add(selButton);
+            midPan.add(butPanel,gbc);
+        }      
+        
         appFrame.setSize(600,400);
         appFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         appFrame.setVisible(true);
         
-    }
  
+        
+    }
     
     
     
