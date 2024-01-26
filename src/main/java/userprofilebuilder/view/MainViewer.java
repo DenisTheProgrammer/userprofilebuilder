cd22237ae61f69306a4713753a1373867c25d1b7
k2206485 <k2206485@PPRSB2022-C6.kuds.kingston.ac.uk>
2024-01-26 11:51:30 +0000

    W1R5 - done some tidying

--- src/main/java/userprofilebuilder/view/MainViewer.java
+++ src/main/java/userprofilebuilder/view/MainViewer.java
@@ -8,6 +8,8 @@ import java.awt.BorderLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
@@ -26,8 +28,43 @@ import userprofilebuilder.model.User;
  * @author 
  * This might be useful for defining you Main App Viewer e.g. a JFrame
  */
-public class MainViewer extends JFrame 
+public class MainViewer extends JFrame implements ActionListener
 {
+    private JPanel namePan = new JPanel();
+    private JPanel titlePan = new JPanel();
+    private JPanel emailPan = new JPanel();
+    
+    
+    //getters and setters
+
+    public JPanel getNamePan() {
+        return namePan;
+    }
+
+    public void setNamePan(JPanel namePan) {
+        this.namePan = namePan;
+    }
+
+    public JPanel getTitlePan() {
+        return titlePan;
+    }
+
+    public void setTitlePan(JPanel titlePan) {
+        this.titlePan = titlePan;
+    }
+
+    public JPanel getEmailPan() {
+        return emailPan;
+    }
+
+    public void setEmailPan(JPanel emailPan) {
+        this.emailPan = emailPan;
+    }
+    
+    //methods
+    
+    
+  
     
     public void myGui(ArrayList<User> myList)
     {
@@ -37,23 +74,20 @@ public class MainViewer extends JFrame
         /*JPanel topPan = new JPanel();
         appFrame.add(topPan);*/
   
-        JPanel namePan = new JPanel();
+        
         namePan.setLayout(new GridBagLayout());
         appFrame.add(namePan,BorderLayout.CENTER);//create a panel that goes in the center of the frame and has GridBagLayout
         
-        JPanel titlePan = new JPanel();
         titlePan.setLayout(new GridBagLayout());
         appFrame.add(titlePan,BorderLayout.CENTER);//create a panel that goes in the center of the frame and has GridBagLayout
-        
-        JPanel emailPan = new JPanel();
+         
         emailPan.setLayout(new GridBagLayout());
         appFrame.add(emailPan,BorderLayout.CENTER);//create a panel that goes in the center of the frame and has GridBagLayout
         
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridwidth = GridBagConstraints.REMAINDER;
         gbc.fill = GridBagConstraints.VERTICAL;
-        gbc.insets = new Insets(5, 5, 5, 5); // these are the constraints for grid bag layout - play with in the future 
-                                                               //to keep building knowledge
+        gbc.insets = new Insets(20, 20, 20, 20); // these are the constraints for grid bag layout - play with in the future 
         
         Border border = BorderFactory.createTitledBorder("Name");
         namePan.setBorder(border);//this is how you set a titled border and add it to a panel or maybe even a frame
@@ -115,12 +149,20 @@ public class MainViewer extends JFrame
         tabs.addTab("Email", emailPan);
         appFrame.add(tabs, BorderLayout.PAGE_START); //tabbed pans, run app to see, this is the top User Name one
         
-        appFrame.setSize(500,230);
+       
+        
+        appFrame.setSize(600,400);
         appFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         appFrame.setVisible(true);//remeber to always set these attributes AT THE END or else app will not display
+
+ 
      
     }
     
-    
+       @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        
+    }
     
 }
